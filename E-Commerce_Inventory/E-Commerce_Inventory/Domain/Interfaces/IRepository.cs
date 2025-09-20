799081a3211c61479b19f024d17ca123a94f5829
ZH_Rifat <99504215+Rifat-H7@users.noreply.github.com>
2025-09-20 23:36:53 +0600

    Add repository and unit of work interfaces and implementation
    Introduces IRepository and IUnitOfWork interfaces to define data access contracts, and adds a generic Repository implementation using Entity Framework Core. These changes establish a foundation for consistent and testable data access patterns in the application.

---
+++ E-Commerce_Inventory/E-Commerce_Inventory/Domain/Interfaces/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq.Expressions;
+
+namespace E_Commerce_Inventory.Domain.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<T?> GetByIdAsync(int id);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
+        Task<T?> FirstOrDefaultAsync(Expression<Func<T, bool>> predicate);
+        Task AddAsync(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        Task<bool> ExistsAsync(Expression<Func<T, bool>> predicate);
+        Task<int> CountAsync(Expression<Func<T, bool>>? predicate = null);
+    }
+}
---
+++ E-Commerce_Inventory/E-Commerce_Inventory/Domain/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using E_Commerce_Inventory.Domain.Entities;
+
+namespace E_Commerce_Inventory.Domain.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IRepository<User> Users { get; }
+        IRepository<RefreshToken> RefreshTokens { get; }
+        IRepository<Category> Categories { get; }
+        IRepository<Product> Products { get; }
+
+        Task<int> SaveChangesAsync();
+        Task BeginTransactionAsync();
+        Task CommitTransactionAsync();
+        Task RollbackTransactionAsync();
+    }
+}

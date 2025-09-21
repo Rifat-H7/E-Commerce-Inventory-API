ec3b22155d7648d1565fe50e1361c85f71bfaece
ZH_Rifat <99504215+Rifat-H7@users.noreply.github.com>
2025-09-21 19:42:29 +0600

    Add service interfaces for Category and Product
    Introduced ICategoryService and IProductService interfaces to define contract methods for CRUD operations on categories and products. These interfaces will facilitate service layer abstraction and improve testability.

---
+++ E-Commerce_Inventory/E-Commerce_Inventory/Application/Services/ICategoryService.cs
@@ -0,0 +1,13 @@
+﻿using E_Commerce_Inventory.Application.DTOs;
+
+namespace E_Commerce_Inventory.Application.Services
+{
+    public interface ICategoryService
+    {
+        Task<ApiResponseDto<CategoryDto>> CreateAsync(CreateCategoryDto createDto);
+        Task<ApiResponseDto<IEnumerable<CategoryDto>>> GetAllAsync();
+        Task<ApiResponseDto<CategoryDto>> GetByIdAsync(int id);
+        Task<ApiResponseDto<CategoryDto>> UpdateAsync(int id, UpdateCategoryDto updateDto);
+        Task<ApiResponseDto<bool>> DeleteAsync(int id);
+    }
+}
---
+++ E-Commerce_Inventory/E-Commerce_Inventory/Application/Services/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using E_Commerce_Inventory.Application.DTOs;
+
+namespace E_Commerce_Inventory.Application.Services
+{
+    public interface IProductService
+    {
+        Task<ApiResponseDto<ProductDto>> CreateAsync(CreateProductDto createDto);
+        Task<ApiResponseDto<PagedResultDto<ProductDto>>> GetAllAsync(ProductQueryDto queryDto);
+        Task<ApiResponseDto<ProductDto>> GetByIdAsync(int id);
+        Task<ApiResponseDto<ProductDto>> UpdateAsync(int id, UpdateProductDto updateDto);
+        Task<ApiResponseDto<bool>> DeleteAsync(int id);
+    }
+}
